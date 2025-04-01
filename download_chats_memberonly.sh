d77a9f264b5224e05883ce0d65b77369656d154f
kenh0u <21225079+kenh0u@users.noreply.github.com>
2025-04-02 02:41:28 +0900

    init

---
+++ download_chats_memberonly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LOG=download_chats_memberonly.log
+
+exec 1> >(
+  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+    | tee -a $LOG
+)
+exec 2> >(
+  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+    | tee -a $LOG
+)
+
+mkdir -p chat_memberonly
+
+while read line
+do
+  videourl='https://youtu.be/'"$line"
+  yt-dlp -o "chat/%(upload_date)s_%(title)s_[%(id)s].%(ext)s" --skip-download --youtube-skip-dash-manifest --write-sub --write-comments --cookies youtube.com_cookies.txt "$videourl"
+done < videoids_memberonly.txt
